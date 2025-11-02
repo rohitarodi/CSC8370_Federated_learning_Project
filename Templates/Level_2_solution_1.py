b71554c6dfd878075026bfffbe6cb0968b232b42
dong yang <151881313+DongYang26@users.noreply.github.com>
2025-11-02 09:03:20 -0500

    update

--- Templates/Level_2_solution_1.py
+++ Templates/Level_2_solution_1.py
@@ -1,5 +1,3 @@
-# we will talk about how to build Federated learning-based model to achieve Image Classification for protecting data privacy on Oct 10 (Thru).
-
 import torch
 import torchvision
 import torchvision.transforms as transforms
