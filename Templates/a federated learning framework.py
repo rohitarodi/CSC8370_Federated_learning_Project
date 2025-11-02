eaf48a38948ca945afea150959e677c418fa81f5
dong yang <151881313+DongYang26@users.noreply.github.com>
2025-11-02 09:07:40 -0500

    update
    Added a brief introduction and usage instructions for the federated learning framework.

--- Templates/a federated learning framework.py
+++ Templates/a federated learning framework.py
@@ -3,4 +3,7 @@ This is a federated learning framework for MNIST classification, on which you ca
 
 The framework provides:
 Pre-implemented data loading functions, evaluation metrics, and the aggregation and distribution of model parameters in federated learning. Based on this framework, you can focus on designing your core functionality/model without worrying about the engineering aspects.
+
+Go ahead: [Level_2_solution_1](https://github.com/DongYang26/CSC8370_25FullDemo/blob/main/Templates/Level_2_solution_1.py)
+
 '''
